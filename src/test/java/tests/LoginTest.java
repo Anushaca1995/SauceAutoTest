b2ee920aa0dc43fd4f5f7931cce71b99f6214336
anusha <anushaca1995@gmail.com>
2025-04-06 18:18:22 +0100

    Explicit wait-selenium

--- src/test/java/tests/LoginTest.java
+++ src/test/java/tests/LoginTest.java
@@ -4,7 +4,11 @@ import org.junit.Assert;
 import org.junit.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import pages.LoginPage;
+
+import java.time.Duration;
 
 public class LoginTest {
     @Test
@@ -16,10 +20,10 @@ public class LoginTest {
         LoginPage loginPage = new LoginPage(driver);
         loginPage.loginMethod("standard_user", "secret_sauce");
 
-        Thread.sleep(2000); // optional
-
-        Assert.assertTrue(driver.getCurrentUrl().contains("inventory"));
-
+        //Thread.sleep(2000); // waits for 2sec
+        //Explicit wait for synchronization using selenium. waits for 10s until inventory url getting. ow goto next line
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        wait.until(ExpectedConditions.urlContains("inventory"));
         driver.quit();
     }
 }
