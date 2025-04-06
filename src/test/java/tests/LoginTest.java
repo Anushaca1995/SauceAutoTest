52b04df8afdd0ca78b6e9084eae70651339067ff
anusha <anushaca1995@gmail.com>
2025-04-06 19:08:51 +0100

    Clean and reusable code. removed jupiter and make all junit annotations

--- src/test/java/tests/LoginTest.java
+++ src/test/java/tests/LoginTest.java
@@ -1,7 +1,10 @@
 package tests;
 
+import org.junit.After;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
@@ -11,19 +14,42 @@ import pages.LoginPage;
 import java.time.Duration;
 
 public class LoginTest {
-    @Test
-    public void testLoginWorks() throws InterruptedException {
-        WebDriver driver = new ChromeDriver();
+    private WebDriver driver;
+    private WebDriverWait wait;
+    @Before
+    public void start(){
+        driver = new ChromeDriver();
         driver.manage().window().maximize();
         driver.get("https://www.saucedemo.com/");
+        wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+    }
 
+    @After
+    public void tearDown(){
+        if (driver!=null){
+            driver.quit();
+        }
+    }
+
+    @Test
+    public void testLoginWorks() throws InterruptedException {
         LoginPage loginPage = new LoginPage(driver);
         loginPage.loginMethod("standard_user", "secret_sauce");
 
         //Thread.sleep(2000); // waits for 2sec
         //Explicit wait for synchronization using selenium. waits for 10s until inventory url getting. ow goto next line
-        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
         wait.until(ExpectedConditions.urlContains("inventory"));
-        driver.quit();
+        Assert.assertTrue("Login failed", driver.getCurrentUrl().contains("inventory"));
     }
+
+    @Test
+
+    public void testLoginInvalidCred(){
+        LoginPage loginPage = new LoginPage(driver);
+        loginPage.loginMethod("abcd", "1234");
+        String error = driver.findElement(By.xpath("//*[@id=\"login_button_container\"]/div/form/div[3]/h3")).getText();
+        System.out.println(error);
+        Assert.assertTrue(error.contains("Username and password do not match"));
+    }
+
 }
